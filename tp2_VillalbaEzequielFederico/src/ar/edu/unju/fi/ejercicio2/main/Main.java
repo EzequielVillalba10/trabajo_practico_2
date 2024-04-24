22e6c3ac22fe293e2abdc163b53fba57e5759094
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-24 17:45:47 -0300

    Creacion de ArrayList para objetos Efemeride

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio2/main/Main.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio2/main/Main.java
@@ -1,7 +1,15 @@
 package ar.edu.unju.fi.ejercicio2.main;
 
+import java.util.List;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio2.model.Efemeride;
+
 public class Main {
 
+	public static Scanner sc;
+	
+	public static List<Efemeride> efemerides;
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
