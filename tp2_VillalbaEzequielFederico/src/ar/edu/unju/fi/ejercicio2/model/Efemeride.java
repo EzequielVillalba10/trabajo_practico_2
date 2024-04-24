520bbbb1f8aec5dfcb76a2a9697718a743d27a7a
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-24 17:33:13 -0300

    Creacion de paquetes, clases y enumerado para el ejercicio 2.

---
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio2.model;
+
+import ar.edu.unju.fi.ejercicio2.constantes.*;
+
+public class Efemeride {
+		private int codigo;
+		private Mes mes;
+		private int dia;
+		private String detalle;
+		
+		
+}
