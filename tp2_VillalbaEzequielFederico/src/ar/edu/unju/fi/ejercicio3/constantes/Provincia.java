7d3298b5cc049c654019b7625c1f055820f2c936
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-25 01:43:15 -0300

    creacion de metodo para calcular la densidad poblacional

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -1,40 +1,34 @@
 package ar.edu.unju.fi.ejercicio3.constantes;
 
+
+
 public enum Provincia {
-	JUJUY, SALTA, TUCUMAN, CATAMARCA, LA_RIOJA, SANTIAGO_DEL_ESTERO;
+	
+	JUJUY(770000, 53219), SALTA(1335000, 155488), TUCUMAN(1687300, 102602), CATAMARCA(415438, 102602), LA_RIOJA(393531, 89680), SANTIAGO_DEL_ESTERO(896461,136351);
 	
 	private int cantidadPoblacion;
 	private int superficie;
-	
 	
 	private Provincia(int cantidadPoblacion, int superficie) {
 		this.cantidadPoblacion = cantidadPoblacion;
 		this.superficie = superficie;
 	}
 
-
 	public int getCantidadPoblacion() {
 		return cantidadPoblacion;
 	}
-
-
-	public void setCantidadPoblacion(int cantidadPoblacion) {
-		this.cantidadPoblacion = cantidadPoblacion;
-	}
-
 
 	public int getSuperficie() {
 		return superficie;
 	}
 
-
-	public void setSuperficie(int superficie) {
-		this.superficie = superficie;
-	}
+	// metodo double calcularDensidad poblacional......
 	
-	public double calcularDensidadDePoblacionDeProvincia(){
+	public double calcularDensidadPoblacional()
+	{
 		return cantidadPoblacion / superficie;
 	}
+
 	
 	
 	
