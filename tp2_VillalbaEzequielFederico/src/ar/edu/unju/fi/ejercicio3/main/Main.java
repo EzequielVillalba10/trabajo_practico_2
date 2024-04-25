7e1145f2a0a84c64eb6e8315f440f674dbb21b30
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-25 01:51:56 -0300

    Creacion de array

---
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio3.main;
+
+import ar.edu.unju.fi.ejercicio3.constantes.Provincia;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Provincia[] provincias = Provincia.values();
+		
+	}
+
+}
