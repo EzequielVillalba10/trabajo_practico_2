6a1cc445de16de305d56bfd8eee60f6ea8f7a5b7
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-26 22:03:44 -0300

    Creacion de paquetes para el ejercicio 4

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.fi.ejercicio4.model;
 
+import java.time.LocalDate;
+
+
 public class Jugador {
 
+		private String nombre;
+		private String apellido;
+		private LocalDate fechaDeNacimiento;
+		private String nacionalidad;
+		private double peso;
+		private double altura;
 }
