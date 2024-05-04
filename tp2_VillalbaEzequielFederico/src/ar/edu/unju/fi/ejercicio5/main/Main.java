82e2fa6297ea470a4be56fa10e68eda8039c2bb0
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-05-04 17:30:29 -0300

    creo el metodo estatico para precargar productos y agregarlo en una arraylist!

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/main/Main.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/main/Main.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 import ar.edu.unju.fi.ejercicio5.model.Producto;
+import ar.edu.unju.fi.ejercicio5.model.Producto.categoria;
+import ar.edu.unju.fi.ejercicio5.model.Producto.origenFabricacion;
 
 public class Main {
 		
@@ -16,9 +18,33 @@ public class Main {
 		// TODO Auto-generated method stub
 		sc = new Scanner(System.in);
 		productos = new ArrayList<>();
-		
+		precargarProductos();
 		
 		
 	}
+	
+	
+	
+	public static void precargarProductos() {
+	if(productos.isEmpty()) {
+		productos.add(new Producto(1, "Heladera Philips", 1000000, origenFabricacion.ARGENTINA, categoria.ELECTROHOGAR, false));
+		productos.add(new Producto(2, "Licuadora P 102", 70000, origenFabricacion.ARGENTINA, categoria.ELECTROHOGAR, false));
+		productos.add(new Producto(3, "Play Station 4", 600000, origenFabricacion.CHINA, categoria.INFORMATICA, true));
+		productos.add(new Producto(4, "Celular Motorola Motog60s", 160000, origenFabricacion.CHINA, categoria.TELEFONIA, true));
+		productos.add(new Producto(5, "Heladera con frezzer Drean", 115999, origenFabricacion.URUGUAY, categoria.ELECTROHOGAR, false));
+		productos.add(new Producto(6, "Tostadora HP Negra 27", 60999, origenFabricacion.BRASIL, categoria.ELECTROHOGAR, true));
+		productos.add(new Producto(7, "Plancha azul glim", 40000, origenFabricacion.BRASIL , categoria.ELECTROHOGAR, true));
+		productos.add(new Producto(8, "Impresora HP 14 negra", 400000, origenFabricacion.ARGENTINA, categoria.INFORMATICA, false));
+		productos.add(new Producto(9, "Celular Samsung S23+", 950000, origenFabricacion.ARGENTINA, categoria.TELEFONIA, true));
+		productos.add(new Producto(10, "Taladro Philco 130W", 31540, origenFabricacion.BRASIL, categoria.HERRAMIENTAS, true));
+		productos.add(new Producto(11, "Maquina soldadora lsc", 130000, origenFabricacion.URUGUAY, categoria.HERRAMIENTAS, false));
+		productos.add(new Producto(12, "Netbook HP mini 210", 430000, origenFabricacion.ARGENTINA, categoria.INFORMATICA, false));
+		productos.add(new Producto(13, "Heladera Philips", 1000000, origenFabricacion.CHINA, categoria.ELECTROHOGAR, true));
+		productos.add(new Producto(14, "Celular Xoami Redmi note 11 pro", 430000, origenFabricacion.CHINA, categoria.TELEFONIA, true));
+		productos.add(new Producto(15, "Caminadora Electrica Duel", 850000, origenFabricacion.CHINA, categoria.ELECTROHOGAR, false));
+		
+	}
+
+}
 
 }
