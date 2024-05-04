b53721c33dba3f3208f60f11ca654348a3826337
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-05-04 16:54:50 -0300

    Creo el objeto scanner y creo la Arraylist para los productos.

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/main/Main.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/main/Main.java
@@ -1,10 +1,24 @@
 package ar.edu.unju.fi.ejercicio5.main;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+import ar.edu.unju.fi.ejercicio5.model.Producto;
+
+public class Main {
+		
+	
+		private static Scanner sc;
+		private static List<Producto> productos;
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		sc = new Scanner(System.in);
+		productos = new ArrayList<>();
+		
+		
+		
 	}
 
 }
