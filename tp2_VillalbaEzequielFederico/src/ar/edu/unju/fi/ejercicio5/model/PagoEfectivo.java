e8b36113db571d9bdf37385bf3f2e17945d89023
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-05-03 22:22:40 -0300

    Creo atributos para pagoTarjeta y su respectivo constructor con los metodos accesores.

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -1,9 +1,11 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
+
 import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
 
 public class PagoEfectivo implements Pago {
-
+	
+	
 	@Override
 	public void realizarPago(double monto) {
 		// TODO Auto-generated method stub
--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
@@ -1,8 +1,22 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
+import java.time.LocalDate;
+
 import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
 
 public class PagoTarjeta implements Pago {
+
+	private long numeroTarjeta;
+	private LocalDate fechaPago;
+	private double montoPagado;
+	
+	
+
+	public PagoTarjeta(long numeroTarjeta, LocalDate fechaPago, double montoPagado) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
 
 	@Override
 	public void realizarPago(double monto) {
@@ -15,5 +29,31 @@ public class PagoTarjeta implements Pago {
 		// TODO Auto-generated method stub
 
 	}
+
+	public long getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(long numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+	
+	
 
 }
