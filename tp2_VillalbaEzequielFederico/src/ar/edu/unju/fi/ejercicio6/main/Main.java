ed5790972f2d9dda28bffdef9846ac103e6fb249
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-05-04 21:31:17 -0300

    Defenicion de expresion para convertir de felinodomestico a felinosalvaje

--- tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,6 +1,8 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import ar.edu.unju.fi.ejercicio6.interfaces.funcionales.Converter;
 import ar.edu.unju.fi.ejercicio6.model.FelinoDomestico;
+import ar.edu.unju.fi.ejercicio6.model.FelinoSalvaje;
 
 public class Main {
 
@@ -8,6 +10,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		FelinoDomestico gato = new FelinoDomestico("Garfield", (byte)45, 12f);
 		
+		//Definicion de expresion lambda que define el convertidor de FelinoDOmestico a FelinoSalvaje
+		
+		Converter<FelinoDomestico, FelinoSalvaje> converter = x -> new FelinoSalvaje(x.getNombre(),x.getEdad(),x.getPeso());
 		
 	}
 
