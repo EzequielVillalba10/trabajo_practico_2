1f302f5a496ef07ca8bd60bd8b776c08ea2d81a7
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-05-04 21:26:44 -0300

    Creacion de main y creacion de objeto FelinoDomestico!

---
+++ tp2_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio6.main;
+
+import ar.edu.unju.fi.ejercicio6.model.FelinoDomestico;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FelinoDomestico gato = new FelinoDomestico("Garfield", (byte)45, 12f);
+		
+		
+	}
+
+}
